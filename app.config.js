ef63ad24a478144979edd2d55f6ad6e4f814e89a
scaraude <ludodante@hotmail.fr>
2026-03-29 22:41:39 +0200

    improve variable check

--- app.config.js
+++ app.config.js
@@ -2,21 +2,35 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-module.exports = ({ config }) => ({
-	...config,
-	android: {
-		...config.android,
-		config: {
-			...config.android?.config,
-			googleMaps: {
-				apiKey: process.env.GOOGLE_MAPS_API_KEY,
+const validateEnvVariables = (variable, isRequired) => {
+	const envValue = process.env[variable];
+	if (!envValue) {
+		if (isRequired) {
+			throw new Error(`Environment variable not set: ${variable}`);
+		} else {
+			console.warn(`Warning: Environment variable not set: ${variable}`);
+		}
+	}
+	return envValue;
+};
+
+module.exports = ({ config }) => {
+	return {
+		...config,
+		android: {
+			...config.android,
+			config: {
+				...config.android?.config,
+				googleMaps: {
+					apiKey: validateEnvVariables('GOOGLE_MAPS_API_KEY', false),
+				},
 			},
 		},
-	},
 	extra: {
 		...config.extra,
-		supabaseUrl: process.env.SUPABASE_URL,
-		supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
-		orsApiKey: process.env.EXPO_PUBLIC_ORS_API_KEY,
+		supabaseUrl: validateEnvVariables('SUPABASE_URL', true),
+		supabaseAnonKey: validateEnvVariables('SUPABASE_ANON_KEY', true),
+		orsApiKey: validateEnvVariables('EXPO_PUBLIC_ORS_API_KEY', true),
 	},
-});
+	};
+};
